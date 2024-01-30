04760f48c469f5d5852d4506d83f012a16369719
mamta <mamta@gmail.com>
2024-01-30 23:04:48 +0530

    Added CheckingGit2

---
+++ src/actionClassdisc/ChekingGit2.java
@@ -0,0 +1,15 @@
+package actionClassdisc;
+
+public class CheckinGit2 {
+	
+	public static void main(String[] args) {
+		
+		int i =10;
+		int j = 5;
+		
+		int k= j/i;
+		
+		System.out.println(k);
+	}
+
+}
